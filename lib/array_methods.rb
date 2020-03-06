0607e070c6563812ba1d9257e04938bbbe7dc34e
Paul Ralyea <047par@gmail.com>
2020-03-06 21:52:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("wow")
-  array.include?("Tardis")
+  (array & ["wow", "Tardis"]).present?
 end
 
 def using_include(array, element)
